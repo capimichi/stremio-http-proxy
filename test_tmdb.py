a292c8a94b31914bf8c73cdfcd0f169b67a307c0
Michele Capichioni <capimichi@gmail.com>
2026-06-28 20:57:17 +0200

    Remove root-level test files

--- test_tmdb.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from stremio_http_proxy.client.tmdb_client import TMDBClient
-
-async def main():
-    client = TMDBClient(api_key="546644f12eb60ad4ddffba8d23428989") # I will use a dummy one or maybe it fails with 401
-    print(await client.get_meta_by_imdb_id("tt0944947", "series"))
-
-asyncio.run(main())
--- test_upstream.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from stremio_http_proxy.client.upstream_client import UpstreamClient
-
-async def main():
-    client = UpstreamClient("https://torrentio.strem.fun/", 10)
-    res = await client.get_json("/stream/series/tt0903747:1:1.json")
-    print(len(res.get("streams", [])))
-
-asyncio.run(main())
